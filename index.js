e62e931983a4959ab9283a7e4ec95f29ea51596f
Cristian dev Web Front End <159144865+CristianTIProgTI@users.noreply.github.com>
2024-03-15 19:48:47 +0000

    1o envio criação objetos

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+var sala = {
+    numero: 59,
+    descricao: 'Descrição?',
+    
+}
+
+var reserva={
+    id: 0,
+    numeroDaSala: sala.numero,
+    data: 0,
+    horaInicial: 0,
+    horaFinal: 0
+}
+
+console.log(reserva.numeroDaSala) //testando
