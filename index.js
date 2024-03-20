3f00ef427d51560494cff52dd25a9198fb249619
Cristian dev Web Front End <159144865+CristianTIProgTI@users.noreply.github.com>
2024-03-19 21:52:57 -0300

    Update index.js
    criação de função para inserção de data inicial de reserva

--- index.js
+++ index.js
@@ -1,16 +1,102 @@
+//este arquivo index.js é o do CRISTIAN
 
-var sala = {
-    numero: 59,
-    descricao: 'Descrição?',
+/* function digitarData (texto){
+//var texto='Início'
+//var textoFin='Final'
+var ano=prompt(`Digite o ANO do ${texto} da reserva`) 
+var mes=prompt(`Digite o MÊS do ${texto} da reserva`)
+mes=mes-1 //porque 0 é jan e 11 é dez
+var dia=prompt(`Digite o DIA do ${texto} da reserva`)
+var hora=prompt(`Digite a HORA do ${texto} da reserva`)
+var minuto=prompt(`Digite o MINUTO do ${texto} da reserva`) //mover as variáves e/ou prompts para fora da função!?
+var data=new Date (ano,mes,dia,hora,minuto)
+data.getFullYear(ano)
+data.setMonth(mes);
+data.setDate(dia)
+data.setHours(hora)
+data.setMinutes(minuto)
+//console.log(data)
+//if (mes='Mar') {console.log('Março')} //continuar criando para cada mês (NÃO PRECISA MAIS!!!)
+let diaSem = data.getDay(data)
+//if (diaSem=1){console.log('Segunda-feira')} ///continuar criando para cada dia da semana (NÃO PRECISA MAIS!!!)
+//criar um console.log com a data em português
+//console.log(diaSem)
+//var outraData=new Date (2024,2,18,10,0) //apagar?
+//console.log(outraData) //apagar?
+//if (outraData>data){console.log('depois')} //criar uma função para comparar horários de reservas (outra função? mover para fora desta?)
+const diaDaSemana = diaDaSemanaPorExtenso(data);
+console.log(diaDaSemana);
+console.log(data.toLocaleDateString('pt-BR'));
+console.log(data.toLocaleTimeString('pt-BR'));
+
+        function diaDaSemanaPorExtenso(data) {
+            const diasDaSemana = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
+            return diasDaSemana[diaSem];
+        }
+ return data ;
+};
+
+function digDataInicial(){digitarData('INÍCIO')} ;
+function digDataFinal(){digitarData('FINAL')};
+var dataInicial=digDataInicial();
+var dataFinal=digDataFinal();
+console.log (dataInicial);
+console.log (dataFinal); */
+//criar outra função para o horário final da reserva (copiar,colar e editar a função acima)
+
+//acima, a primeira versão da função (poderá ser apagada depois) ^^^^^^^^^^^--------------------------------------------------------------------------
+
+
+
+
+function digitarDataInicial (ano,mes,dia,hora,minuto){ //função para receber data e hora do início da reserva 
+    mes=mes-1 //porque 0 é jan e 11 é dez
+    var data=new Date (ano,mes,dia,hora,minuto) //Cria uma instância de data
+    data.getFullYear(ano); //retorna o ano da data especificada 
+    data.setMonth(mes); //atribui o mês para uma data específica
+    data.setDate(dia); //atribui o dia do mês pra uma data específica
+    data.setHours(hora); //atribui as horas para uma data especificada
+    data.setMinutes(minuto); //atribui os minutos para uma data específica
+    return data ; //resultado da função para ser usada depois no objeto 'reseva' no formato completo de data (Tue Mar 19 2024 18:48:23 GMT-0300 (GMT-03:00))
+    };
     
+    
+var ano=prompt(`Digite o ANO do INÍCIO da reserva`) ; //prompts seguintes para o usuário digitar a data e a hora inicial da reserva
+var mes=prompt(`Digite o MÊS do INÍCIO da reserva`);
+var dia=prompt(`Digite o DIA do INÍCIO da reserva`);
+var hora=prompt(`Digite a HORA do INÍCIO da reserva`);
+var minuto=prompt(`Digite o MINUTO do INÍCIO da reserva`)   
+    
+var dataInicial=digitarDataInicial(ano,mes,dia,hora,minuto); //cria a variável dataInicial a partir da função e dos dados digitados pelo usuário
+console.log (dataInicial); //exibe a data inicial no formato completo (é só pra teste, pode-se apagar esta linha depois)
+    
+function diaDaSemanaPorExtenso(dataInicial) { //função (dentro da função) para transformar o número do dia da semana em texto
+        const diasDaSemana = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
+        return diasDaSemana[diaSem]; //diaSem é um número de 0 a 6 dado por getDay
+    }
+    
+let diaSem = dataInicial.getDay(dataInicial) //getDay retorna o dia da semana para a data especificada (número de 0 a 6, sendo 0 o domingo) e passa para a var para ser usado na função que retora o nome do dia da semana
+const diaDaSemana = diaDaSemanaPorExtenso(dataInicial); //cria a variável com o nome do dia da semana por extenso a partir do resultado da função para depois ser exibido ao usuário
+var dataBR=(dataInicial.toLocaleDateString('pt-BR')); //variável com a data no formato dd/mm/aaaa para ser exibida ao usuário
+var horaBR=(dataInicial.toLocaleTimeString('pt-BR')); //variável com a hora no formato hh:mm:ss para ser exibida ao usuário
+console.log(`Sua reserva inicia no(a) ${diaDaSemana}, ${dataBR}, às ${horaBR}`) //exibe a data e hora do início da reserva
+//criar uma função mãe com tudo isso acima dentro, inclusive 'digitarDataInicial'!? acho q sim!
+
+
+var reserva={ //objeto q vai armazenar uma reserva
+    idRes: [0], //acho q aí deve haver um array q vai incrementando a medida que as reservas vão sendo criadas (ver como fazer)
+    numeroQuarto: Quarto.id, //puxa do objeto 'Quarto' do Rafael (ver como fazer)
+    horaInicial: dataInicial, //puxa da função 'digitarDataInicial' ou uma acima dela (ver como fazer)
+    horaFinal: dataFinal//criar ainda uma função para o final da reserva (fazer uma cópia de 'digitarDataInicial', chamar de 'digitarDataFinal' e mudar o nome de 'data' para 'dataFinal'. Precisa mudar os nomes de todas as var dentro da função ??? acho q não!)
 }
 
-var reserva={
-    id: 0,
-    numeroDaSala: sala.numero,
-    data: 0,
-    horaInicial: 0,
-    horaFinal: 0
-}
+//criar uma função para comparar os horários de diferentes reservas para evitar conflitos de agenda!!!s
 
-console.log(reserva.numeroDaSala) //testando
+//objeto do Rafael (somente para consultar os nomes dos parâmetros e poder repetí-los no objeto 'reserva')
+/* class Quarto {
+    constructor(id, tipo, descricao) {
+        this.id = id;
+        this.tipo = tipo;
+        this.descricao = descricao;
+    }
+} */
